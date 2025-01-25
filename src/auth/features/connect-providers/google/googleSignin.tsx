f85224b790f07743bc43725eb907c3f97a50d634
Aldrin Caballero <caballeroaldrin02@gmail.com>
2025-01-25 16:55:56 -0700

    Fixed google signin

--- src/auth/features/connect-providers/google/googleSignin.tsx
+++ src/auth/features/connect-providers/google/googleSignin.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { GoogleLogin, CredentialResponse } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 
@@ -9,32 +8,18 @@ type DecodedToken = {
 };
 
 export const GoogleSignin = () => {
-  const [user, setUser] = useState<DecodedToken | null>(null);
-
   const responseMessage = (response: CredentialResponse) => {
     if (response.credential) {
       try {
         // Decode the JWT credential to get user information
         const decoded: DecodedToken = jwtDecode(response.credential);
-        console.log("Decoded User:", decoded);
-        setUser(decoded);
+        console.log("User:", decoded);
+        
       } catch (error) {
         console.error("Failed to decode token:", error);
       }
     }
   };
 
-  return (
-    <div>
-      {!user ? (
-        <GoogleLogin onSuccess={responseMessage} />
-      ) : (
-        <div>
-          <h3>Welcome, {user.name}!</h3>
-          <img src={user.picture} alt={`${user.name}'s profile`} />
-          <p>Email: {user.email}</p>
-        </div>
-      )}
-    </div>
-  );
+  return <GoogleLogin onSuccess={responseMessage} />;
 };
